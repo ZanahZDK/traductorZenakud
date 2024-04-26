36fd53c3b92e3a6b0cb945b46c1f42c4bac10f4c
Zanah <die.sagredo@duocuc.cl>
2024-04-25 21:43:04 -0400

    add flask configs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -65,4 +66,5 @@ def traducir():
     return jsonify({'traduccion': traduccion})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
